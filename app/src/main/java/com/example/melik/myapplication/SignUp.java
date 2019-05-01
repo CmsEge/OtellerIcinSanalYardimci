4d3e9852645de507f4744dad12a0f4bc57f70485
melikegulas <melikegulas@gmail.com>
2019-05-01 13:36:04 +0300

    layouts

--- app/src/main/java/com/example/melik/myapplication/SignUp.java
+++ app/src/main/java/com/example/melik/myapplication/SignUp.java
@@ -1,30 +1,23 @@
 package com.example.melik.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ViewAnimator;
 
-
-public class SignIn extends AppCompatActivity {
+public class SignUp  extends AppCompatActivity {
 
     private Button signUp;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.log_in);
+        setContentView(R.layout.sign_up);
         signUp=findViewById(R.id.signUpButton);
     }
     public void SignUp(View v){
-        Intent intent=new Intent(SignIn.this,SignUp.class);
-        startActivity(intent);
-    }
-    public void SignIn(View v){
-        Intent intent=new Intent(SignIn.this,MainScreen.class);
+        Intent intent=new Intent(SignUp.this,SignIn.class);
         startActivity(intent);
     }
 }

e1799485e7fe1fb9143790fac2e08d36485eff07
melikegulas <melikegulas@gmail.com>
2019-05-09 20:38:00 +0300

    layout

--- app/src/main/java/com/example/melik/myapplication/MainScreen.java
+++ app/src/main/java/com/example/melik/myapplication/MainScreen.java
@@ -7,17 +7,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.melik.eventbrite.Configuration;
 import com.example.melik.places.PlaceMain;
 import com.example.melik.service.Service;
 import com.example.melik.database.Database;
 
+import java.util.ArrayList;
+
 public class MainScreen extends AppCompatActivity {
 
     private Service service;
     private Database database;
     private Button shire;
+    private TextView text;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +29,9 @@ public class MainScreen extends AppCompatActivity {
         shire=findViewById(R.id.shireButton);
         database=new Database(getApplicationContext());
         service= new Service(database);
-
+        text=findViewById(R.id.textView);
+        ArrayList<String> cust=service.getCustomerbyStatus();
+        text.append("Welcome "+ cust.get(1));
     }
     public void ChatBot(View v){
         Intent intent=new Intent(MainScreen.this,MainActivity.class);
